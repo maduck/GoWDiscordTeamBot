c03e0e9765f3367f37159dfcb81b27a522269851
maduck <utti+github@uttisoft.de>
2020-07-18 23:10:57 +0200

    add language file updater

---
+++ update_lang_files.py
@@ -0,0 +1,18 @@
+import gzip
+
+import requests
+
+languages = (
+    'English',
+    'German',
+    'Russian',
+)
+base_url = 'https://gemsofwarconsole.blob.core.windows.net/data/Production480/Localization'
+
+for lang in languages:
+    filename = f'GemsOfWar_{lang}.json'
+    url = f'{base_url}/{filename}.gz'
+    r = requests.get(url)
+    uncompressed = gzip.decompress(r.content)
+    with open(filename, 'wb') as f:
+        f.write(uncompressed)
