8887d9820526b780b62ce1e4d1ef9de8f7b1f850
maduck <utti+github@uttisoft.de>
2020-07-18 22:59:25 +0200

    add initial bot code

---
+++ bot.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import logging
+import os
+
+import discord
+
+from team_expando import TeamExpander
+
+TOKEN = os.getenv('DISCORD_TOKEN', 'NzMzMzk5MDUxNzk3NzkwODEw.XxCz3Q.kYbLJJFO0332ccp7ltqusDwUgJU')
+LOGLEVEL = logging.DEBUG
+
+formatter = logging.Formatter('%(asctime)-15s [%(levelname)s] %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+handler.setLevel(LOGLEVEL)
+log = logging.getLogger(__name__)
+
+log.setLevel(logging.DEBUG)
+log.addHandler(handler)
+
+
+class DiscordBot(discord.Client):
+    BOT_NAME = 'Garys GoW Team Bot'
+    BASE_GUILD = 'GoW Bot Dev'
+    VERSION = '0.1'
+
+    def __init__(self, *args, **kwargs):
+        log.debug(f'--------------------------- Starting {self.BOT_NAME} v{self.VERSION} --------------------------')
+        super().__init__(*args, **kwargs)
+        self.permissions = self.generate_permissions()
+        self.invite_url = 'https://discordapp.com/api/oauth2/authorize?client_id={{}}&scope=bot&permissions={}'
+        self.invite_url = self.invite_url.format(self.permissions.value)
+        self.my_emojis = {}
+        self.expander = TeamExpander()
+
+    @staticmethod
+    def generate_permissions():
+        permissions = discord.Permissions.none()
+        needed_permissions = [
+            'add_reactions',
+            'read_messages',
+            'send_messages',
+            'manage_messages',
+            'read_message_history',
+            'external_emojis',
+        ]
+        for perm_name in needed_permissions:
+            setattr(permissions, perm_name, True)
+        log.debug(f'Permissions required: {", ".join([p for p, v in permissions if v])}')
+        return permissions
+
+    async def on_ready(self):
+        self.invite_url = self.invite_url.format(self.user.id)
+        log.debug(f'Logged in as {self.user.name}')
+        log.info(f'Invite with: {self.invite_url}')
+        log.debug(f'Active in {", ".join([g.name for g in self.guilds])}')
+        for guild in self.guilds:
+            if guild.name == self.BASE_GUILD:
+                for emoji in guild.emojis:
+                    self.my_emojis[emoji.name] = str(emoji)
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        if "[" in message.content:
+            team = self.expander.get_team_from_message(message.content)
+            if not team:
+                log.debug(f'nothing found in message {message.content}')
+                return
+            log.debug(f'[{message.guild}][{message.channel}] sending result to {message.author.display_name}: {team}')
+            color = discord.Color.from_rgb(19, 227, 246)
+            author = message.author.display_name
+            if author[-1] == 's':
+                author += "'"
+            else:
+                author += "'s"
+            e = discord.Embed(title=f"{author} team", color=color)
+            troops = [f'{self.my_emojis.get(t[0], f":{t[0]}:")} {t[1]}' for t in team['troops']]
+            team_text = '\n'.join(troops)
+            e.add_field(name=team['troops_title'], value=team_text, inline=True)
+            banner_texts = [f'{self.my_emojis.get(d[0], f":{d[0]}:")} {abs(d[1]) * f"{d[1]:+d}"[0]}' for d in
+                            team['banner']['description']]
+            e.add_field(name=team['banner']['name'], value='\n'.join(banner_texts), inline=True)
+            if team['class']:
+                e.add_field(name=f'{team["class_title"]}: {team["class"]}', value='\n'.join(team['talents']),
+                            inline=False)
+            await message.channel.send(embed=e)
+
+
+if __name__ == '__main__':
+    client = DiscordBot()
+    client.run(TOKEN)
---
+++ team_expando.py
@@ -0,0 +1,162 @@
+import json
+import logging
+import operator
+import re
+
+LOGLEVEL = logging.DEBUG
+
+formatter = logging.Formatter('%(asctime)-15s [%(levelname)s] %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+handler.setLevel(LOGLEVEL)
+log = logging.getLogger(__name__)
+
+log.setLevel(LOGLEVEL)
+log.addHandler(handler)
+
+
+class Translations:
+    BASE_LANG = 'en'
+    LANGUAGES = {
+        'en': 'English',
+        'de': 'German',
+        'ru': 'Russian',
+    }
+
+    def __init__(self):
+        self.translations = {}
+        for lang_code, language in self.LANGUAGES.items():
+            filename = f'GemsOfWar_{language}.json'
+            with open(filename, encoding='utf8') as f:
+                self.translations[lang_code] = json.load(f)
+
+    def get(self, key, lang=''):
+        if lang not in self.translations:
+            lang = self.BASE_LANG
+
+        return self.translations[lang].get(key, key)
+
+
+class TeamExpander:
+    FORMAT = re.compile(r'(de|en|ru)?\[(\d+,?){1,13}\]', re.IGNORECASE)
+    COLORS = ('blue', 'green', 'red', 'yellow', 'purple', 'brown')
+
+    def __init__(self):
+        self.troops = {}
+        self.weapons = {}
+        self.classes = {}
+        self.banners = {}
+        self.traits = {}
+        self.talent_trees = {}
+        self.translations = Translations()
+        self.populate_world_data()
+
+    def populate_world_data(self):
+        with open('World.json', encoding='utf8') as f:
+            data = json.load(f)
+
+        for troop in data['Troops']:
+            colors = [c.replace('Color', '').lower() for c, v in troop['ManaColors'].items() if v]
+            self.troops[troop['Id']] = {
+                'name': troop['Name'],
+                'colors': sorted(colors),
+            }
+        for kingdom in data['Kingdoms']:
+            color_lookup_names = [f'[GEM_{c.upper()}]' for c in self.COLORS]
+            color_names = [self.translations.get(c).lower() for c in color_lookup_names]
+            colors = zip(color_names, kingdom['BannerColors'])
+            colors = sorted(colors, key=operator.itemgetter(1), reverse=True)
+            self.banners[kingdom['Id']] = {
+                'name': kingdom['BannerName'],
+                'colors': colors,
+            }
+        for weapon in data['Weapons']:
+            colors = [c.replace('Color', '').lower() for c, v in weapon['ManaColors'].items() if v]
+            self.weapons[weapon['Id']] = {
+                'name': f'[SPELL{weapon["SpellId"]}_NAME]',
+                'colors': sorted(colors),
+            }
+        for trait in data['Traits']:
+            self.traits[trait['Code']] = trait['Name']
+        for tree in data['TalentTrees']:
+            talents = [self.traits.get(trait, trait) for trait in tree['Traits']]
+            self.talent_trees[tree['Code']] = talents
+        for _class in data['HeroClasses']:
+            self.classes[_class['Id']] = {
+                'name': _class['Name'],
+                'talents': [self.talent_trees[tree] for tree in _class['TalentTrees']]
+            }
+
+    @classmethod
+    def extract_code_from_message(cls, message):
+        m = cls.FORMAT.search(message)
+        if m is None:
+            return None, None
+        span = m.span()
+        beginning = span[0] + message[span[0]:].find('[') + 1
+        ending = span[1] - 1
+        raw_code = message[beginning:ending]
+        lang = 'en'
+        if message[span[0]] != '[':
+            lang = message[span[0]:span[0] + 2].lower()
+        numbers = [int(n.strip()) for n in raw_code.split(',')]
+        return numbers, lang
+
+    def get_team_from_code(self, code, lang):
+        result = {
+            'troops': [],
+            'banner': {},
+            'class': None,
+            'talents': [],
+            'class_title': self.translations.get('[CLASS]', lang),
+            'troops_title': self.translations.get('[TROOPS]', lang),
+        }
+
+        for element in code:
+            troop = self.troops.get(element)
+            weapon = self.weapons.get(element)
+            if troop:
+                color_code = "".join(troop["colors"])
+                troop_name = self.translations.get(troop['name'], lang)
+                result['troops'].append([color_code, troop_name])
+                continue
+            elif weapon:
+                color_code = "".join(weapon["colors"])
+                weapon_name = self.translations.get(weapon['name'], lang)
+                result['troops'].append([color_code, weapon_name + ' :crossed_swords:'])
+                continue
+
+            _class = self.classes.get(element)
+            if _class:
+                result['class'] = self.translations.get(_class['name'], lang)
+                result['class_talents'] = _class['talents']
+                continue
+
+            banner = self.banners.get(element)
+            if banner:
+                result['banner']['name'] = self.translations.get(banner['name'], lang)
+                result['banner']['description'] = [c for c in banner['colors'] if c[1]]
+                continue
+
+            if 0 <= element <= 3:
+                result['talents'].append(element)
+                continue
+
+        for i in range(7 - len(result['talents'])):
+            result['talents'].append(0)
+
+        new_talents = []
+        for talent_no, talent_code in enumerate(result['talents']):
+            talent = '-'
+            if talent_code > 0:
+                talent = self.translations.get(result['class_talents'][talent_code - 1][talent_no], lang)
+            new_talents.append(talent)
+        result['talents'] = new_talents
+
+        return result
+
+    def get_team_from_message(self, message):
+        code, lang = self.extract_code_from_message(message)
+        if not code:
+            return
+        return self.get_team_from_code(code, lang)
