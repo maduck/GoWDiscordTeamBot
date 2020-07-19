bd2c783eaaa617d63c47a121f0e14197ae1f49c8
maduck <utti+github@uttisoft.de>
2020-07-19 19:05:57 +0200

    fix: provide weapon role title

--- team_expando.py
+++ team_expando.py
@@ -268,6 +268,7 @@ class TeamExpander:
             'name': self.translations.get(spell['name'], lang),
             'description': self.translations.get(spell['description'], lang),
         }
+        troop['roles_title'] = self.translations.get('[WEAPON_ROLE]', lang)
         weapon['roles'] = [self.translations.get(role, lang) for role in weapon['roles']]
         weapon['type_title'] = self.translations.get('[FILTER_TROOPTYPE]', lang)
         weapon['type'] = self.translations.get(f'[WEAPONTYPE_{weapon["type"]}]', lang)
