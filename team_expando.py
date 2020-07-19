7b7a79890721e2cdf5d93f9cd7b35641a200ca55
maduck <utti+github@uttisoft.de>
2020-07-19 19:12:53 +0200

    fix: use weapon type instead of troop type title

--- team_expando.py
+++ team_expando.py
@@ -270,5 +270,5 @@ class TeamExpander:
         }
         weapon['roles_title'] = self.translations.get('[WEAPON_ROLE]', lang)
         weapon['roles'] = [self.translations.get(role, lang) for role in weapon['roles']]
-        weapon['type_title'] = self.translations.get('[FILTER_TROOPTYPE]', lang)
+        weapon['type_title'] = self.translations.get('[FILTER_WEAPONTYPE]', lang)
         weapon['type'] = self.translations.get(f'[WEAPONTYPE_{weapon["type"].upper()}]', lang)
