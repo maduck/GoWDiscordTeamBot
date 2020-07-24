6afae7f17510ae8bac6ee124c71bfffd4014177b
maduck <utti+github@uttisoft.de>
2020-07-24 22:56:20 +0200

    feat: add french help text

--- help.py
+++ help.py
@@ -34,6 +34,40 @@ help_texts = {
                           'can do that.\n'
                           '__Short help__: enter `{0}quickhelp` to open a short overview of all commands.'
     },
+    'fr': {
+        'Codes d\'équipe': '• __Les bases__: Postez simplement votre code d\'équipe, par exemple: [1075,6251,6699,'
+                           '6007,3010,3,1,1,1,3,1,1,14007]. Le bot répondra automatiquement en affichant les troupes '
+                           'postées dans le code. Ce code peut être intégré dans du texte supplémentaire et il ne '
+                           'nécessite pas d\'être seul sur une ligne.\n\n '
+                           '• __Support linguistique__: Toutes les langues utilisées dans GoW sont supportées. '
+                           'Préfixez simplement votre code avec les deux lettres de votre code pays (en, fr, de, ru, '
+                           'it, es, cn),par exemple: fr[1075,6251,6699,6007,3010,3,1,1,1,3,1,1,14007]\n\n '
+                           '• __Format raccourci__: Utilisez le caractère "-" (tiret) en début de code pour que le '
+                           'résultat apparaisse en mode minimal et condensé, par exemple -[1075,6251,6699,6007], '
+                           'ou avec le code langue fr-[1075,6251,6699,6007].',
+        'Recherches': '• __Les bases__: les recherches suivantes sont supportées:\n'
+                      ' - `{0}troop <recherche>`, par exemple `fr{0}troop élémaugrim`.\n'
+                      ' - `{0}weapon <recherche>`\n'
+                      ' - `{0}pet <recherche>`\n'
+                      ' - `{0}class <recherche>`\n'
+                      ' - `{0}kingdom <recherche>`\n'
+                      ' - `{0}talent <recherche>`\n'
+                      '• __Règles__:\n'
+                      '  - La recherche fonctionne avec les numéros ids et les parties de noms.\n'
+                      '  - La recherche n\'est sensible ni aux majuscules ni aux minuscules.\n'
+                      '  - Les espaces, les apostrophes (\') et les tirets (-) peuvent être ignorés.\n\n'
+                      '  - Plusieurs résultats peuvent être affichés, en tant que troupes, s\'ils correspondent '
+                      'à la recherche.\n '
+                      'Si une seule troupe correspond à la recherche effectuée, la couleur du bord du résultat '
+                      'montrera la rareté de base de la troupe.\n\n '
+                      '• __Support linguistique__: Toutes les langues utilisées dans GoW sont supportées. Préfixez '
+                      'simplement votre code avec les deux lettres de votre code pays (en, fr, de, ru, it, es, '
+                      'cn). Les recherches dans la langue correspondante s\'effectueront uniquement sur les noms '
+                      'de troupes dans la langue choisie.',
+        'Préfixe': '• __Les Bases__: tapez `{0}prefix <nouveau_préfixe>` pour configurer un nouveau '
+                   'préfixe. Seul le propriétaire du serveur peut faire ce changement.',
+        'Aide rapide': '• __Les Bases__: tapez `{0}quickhelp` pour ouvrir un court aperçu de toutes les commandes.\n'
+    },
 }
 
 
