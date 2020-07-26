13fe979a15046b297ab3c552e896b569e6de421c
maduck <utti+github@uttisoft.de>
2020-07-26 13:56:39 +0200

    fix: team code f*ckup

--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ class DiscordBot(discord.Client):
         {
             'function': 'handle_team_code',
             'pattern': re.compile(
-                r'.*?(?P<lang>en|fr|de|ru|it|es|cn)?(?P<shortened>-)?\[(?P<team_code>(\d+,?){1,12})\].*',
+                r'.*?(?P<lang>en|fr|de|ru|it|es|cn)?(?P<shortened>-)?\[(?P<team_code>(\d+,?){1,13})\].*',
                 re.IGNORECASE)
         },
         {
