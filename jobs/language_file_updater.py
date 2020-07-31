96ad501b4b089c56358bf7bca07c33117e375619
maduck <utti+github@uttisoft.de>
2020-07-31 07:01:06 +0200

    make language file updater executable

--- jobs/language_file_updater.py
+++ jobs/language_file_updater.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import gzip
 
 import requests
@@ -15,8 +16,10 @@ base_url = 'https://gemsofwarconsole.blob.core.windows.net/data/Production500/Lo
 
 for lang in languages:
     filename = f'GemsOfWar_{lang}.json'
+    print(f'[DEBUG] Downloading language file {filename}.gz...')
     url = f'{base_url}/{filename}.gz'
     r = requests.get(url)
+    print(f'[DEBUG] Decompressing into {filename}...')
     uncompressed = gzip.decompress(r.content)
     with open(filename, 'wb') as f:
         f.write(uncompressed)
