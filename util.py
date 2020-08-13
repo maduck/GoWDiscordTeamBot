a97a1e76f93b1550ef442e2a8be16afb843d284a
maduck <utti+github@uttisoft.de>
2020-08-13 19:56:17 +0200

    refactor: remove unused elif branch in util.py

--- util.py
+++ util.py
@@ -28,14 +28,9 @@ def merge(a, b, path=None):
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
                 merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass  # same leaf value
             else:
-                # Use a for conflicts.
                 pass
-                # raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
         else:
-            # print(f"FILL: {key} : {b[key]}")
             a[key] = b[key]
     return a
 
