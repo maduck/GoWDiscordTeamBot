b78b8674b53c0c253e4e3d4f0fb5e374af52b5f9
maduck <utti+github@uttisoft.de>
2020-08-13 20:03:10 +0200

    refactor: rename subscriptions class variable

--- subscriptions.py
+++ subscriptions.py
@@ -7,14 +7,14 @@ class Subscriptions:
     SUBSCRIPTION_CONFIG_FILE = 'subscriptions.json'
 
     def __init__(self):
-        self.subscriptions = {}
+        self._subscriptions = {}
         self.load_subscriptions()
 
     def save_subscriptions(self):
         lock = threading.Lock()
         with lock:
             with open(self.SUBSCRIPTION_CONFIG_FILE, 'w') as f:
-                json.dump(self.subscriptions, f, sort_keys=True, indent=2)
+                json.dump(self._subscriptions, f, sort_keys=True, indent=2)
 
     def load_subscriptions(self):
         if not os.path.exists(self.SUBSCRIPTION_CONFIG_FILE):
@@ -22,7 +22,7 @@ class Subscriptions:
         lock = threading.Lock()
         with lock:
             with open(self.SUBSCRIPTION_CONFIG_FILE) as f:
-                self.subscriptions = json.load(f)
+                self._subscriptions = json.load(f)
 
     @staticmethod
     def get_subscription_id(guild, channel):
@@ -41,25 +41,25 @@ class Subscriptions:
 
     def add(self, guild, channel):
         s_id, subscription = self.get_subscription(guild, channel)
-        if s_id in self.subscriptions:
-            self.subscriptions[s_id]['pc'] = True
+        if s_id in self._subscriptions:
+            self._subscriptions[s_id]['pc'] = True
         else:
-            self.subscriptions[s_id] = subscription
+            self._subscriptions[s_id] = subscription
         self.save_subscriptions()
 
     def remove(self, guild, channel):
         s_id, subscription = self.get_subscription(guild, channel)
         if self.is_subscribed(guild, channel):
-            self.subscriptions[s_id]['pc'] = False
-            self.subscriptions[s_id]['switch'] = False
+            self._subscriptions[s_id]['pc'] = False
+            self._subscriptions[s_id]['switch'] = False
             self.save_subscriptions()
 
     def is_subscribed(self, guild, channel):
         subscription = self.get_subscription_id(guild, channel)
-        return subscription in self.subscriptions
+        return subscription in self._subscriptions
 
     def __len__(self):
-        return len(self.subscriptions)
+        return len(self._subscriptions)
 
     def __iter__(self):
-        yield from self.subscriptions.values()
+        yield from self._subscriptions.values()
