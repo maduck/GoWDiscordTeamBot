8416f18ddc264859d0805b412f708de7a3d6ac87
Johannes Eimer <johannes.eimer@jep-dev.com>
2020-08-15 15:52:13 +0200

    fix: remove unnecessary lines

--- game_assets.py
+++ game_assets.py
@@ -6,12 +6,9 @@ from config import CONFIG
 class GameAssets:
     @staticmethod
     def load(filename):
-        content = ''
         path = os.path.join(CONFIG.get('game_assets'), filename)
         with open(path, encoding='utf8') as f:
-            content = json.load(f)
-
-        return content
+            return json.load(f)
 
     @staticmethod
     def path(filename):
