e51bd3afdbd1a8c801e6cf47939a2804fc8a0365
maduck <utti+github@uttisoft.de>
2020-10-05 20:13:53 +0200

    fix: campaign task color, attempt #2

--- team_expando.py
+++ team_expando.py
@@ -542,7 +542,7 @@ class TeamExpander:
     def translate_campaign_task(self, task, lang):
         new_task = task.copy()
         color_code = int(new_task['value1']) if new_task['value1'].isdigit() else 0
-        color = COLORS[color_code].upper()
+        color = COLORS[color_code].upper() if color_code <= len(COLORS) else ''
         if isinstance(new_task.get('y'), str):
             new_task['y'] = _(f'[{new_task["y"].upper()}]', lang)
 
