1ca9063740f9ad671a780f81bebe7faa8574feb8
maduck <utti+github@uttisoft.de>
2020-11-17 21:58:49 +0100

    feat: bump version

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 class DiscordBot(BaseBot):
     BOT_NAME = 'garyatrics.com'
-    VERSION = '0.17.1'
+    VERSION = '0.17.2'
     NEEDED_PERMISSIONS = [
         'add_reactions',
         'read_messages',
