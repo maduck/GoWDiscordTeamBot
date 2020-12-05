83efa520c6a946700f10fd6b317920783a2c6e8a
maduck <utti+github@uttisoft.de>
2020-12-05 11:27:32 +0100

    feat: add ⛔ reaction to indicate the bot could not delete a message

--- base_bot.py
+++ base_bot.py
@@ -129,8 +129,7 @@ class BaseBot(discord.Client):
         if payload.member.display_name != message.embeds[0].author.name:
             return
         if not permissions.manage_messages:
-            log.warn(f'[{message.guild}][{message.channel}][{payload.member.display_name}] '
-                     'Missing permissions to delete message')
+            await message.add_reaction('⛔')
             return
 
         await message.clear_reaction(payload.emoji)
