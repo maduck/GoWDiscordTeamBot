84eb2e2cfe3e3c19d764939420d358d40a31072e
maduck <utti+github@uttisoft.de>
2021-01-15 09:35:11 +0100

    feat: support pet food in adventure board

--- data_source/game_data.py
+++ data_source/game_data.py
@@ -572,6 +572,8 @@ class GameData:
                     reward_type = f'[DEED{data:02d}]'
                 elif reward_type == '[RUNE]':
                     reward_type = f'[RUNE{data:02d}_NAME]'
+                elif reward_type == '[PETFOOD]':
+                    reward_type = f'[PETFOOD{data:02d}_NAME]'
                 elif reward_type == '[KEY]':
                     reward_type = f'[KEYTYPE_{data}_TITLE]'
                 result.setdefault(reward_type, 0)
