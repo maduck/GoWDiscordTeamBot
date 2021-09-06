166b9388678ce63217ef1f82a10d22f7bbde597a
maduck <utti+github@uttisoft.de>
2021-09-06 19:51:41 +0200

    feat: support new heroic gems

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 class DiscordBot(BaseBot):
     BOT_NAME = 'garyatrics.com'
-    VERSION = '0.60.8'
+    VERSION = '0.60.9'
     NEEDED_PERMISSIONS = [
         'add_reactions',
         'read_messages',
