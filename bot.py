c5a7854e954074eb14e5ab10f0f3150287c60399
maduck <utti+github@uttisoft.de>
2022-05-07 20:54:28 +0200

    refactor: more array entries for future features

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 class DiscordBot(BaseBot):
     BOT_NAME = 'garyatrics.com'
-    VERSION = '0.75.1'
+    VERSION = '0.75.2'
     NEEDED_PERMISSIONS = [
         'add_reactions',
         'read_messages',
