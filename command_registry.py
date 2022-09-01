cf0b70000a267f5978de9ccad2917c318a2cf39a
maduck <utti+github@uttisoft.de>
2022-09-01 19:22:33 +0200

    feat: shortened flag for class

--- command_registry.py
+++ command_registry.py
@@ -297,7 +297,7 @@ COMMAND_REGISTRY = [
         'function': 'class_',
         'pattern': re.compile(SEARCH_PATTERN.format('class'), MATCH_OPTIONS),
         'description': 'Search classes',
-        'options': [STANDARD_OPTIONS['search_term'], STANDARD_OPTIONS['lang']],
+        'options': [STANDARD_OPTIONS['search_term'], STANDARD_OPTIONS['lang'], STANDARD_OPTIONS['shortened']],
     },
     {
         'function': 'talent',
