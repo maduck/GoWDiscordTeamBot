8222157cb28b63134c917e4cef42cde17ee0eeae
maduck <utti+github@uttisoft.de>
2022-10-26 09:55:46 +0200

    fix: Nimhain's news avatar

--- views.py
+++ views.py
@@ -489,7 +489,7 @@ class Views:
             'Nimhain': {
                 'url': 'https://community.gemsofwar.com/u/Nimhain',
                 'icon_url': 'https://sjc3.discourse-cdn.com/business5/'
-                            'user_avatar/community.gemsofwar.com/nimhain/360/47049.png'},
+                            'user_avatar/community.gemsofwar.com/nimhain/360/47049_2.png'},
             'Saltypatra': {
                 'url': 'https://community.gemsofwar.com/u/saltypatra/',
                 'icon_url': 'https://sjc3.discourse-cdn.com/business5/'
