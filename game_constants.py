ad2a162fa40472ed2c2a59b44a1048687534926e
maduck <utti+github@uttisoft.de>
2023-02-12 09:52:18 +0100

    feat: add orb constants

--- game_constants.py
+++ game_constants.py
@@ -313,3 +313,27 @@ class RewardTypes(enum.IntEnum):
     LiveEventPoints = 86
     TeamSlot = 87
     Banner = 88
+
+
+# public enum OrbType
+class OrbType(enum.IntEnum):
+    Growth = 0
+    GrowthMajor = 1
+    Ascension = 2
+    AscensionMajor = 3
+    Wisdom = 4
+    WisdomMajor = 5
+    Clans = 6
+    ClansMajor = 7
+    Chaos = 8
+    ChaosMajor = 9
+    Power = 10
+    Forging = 11
+    ForgingMajor = 12
+    Ingots = 13
+    IngotsMajor = 14
+    Minions = 15
+    MinionsMajor = 16
+    GloryLesser = 17
+    Glory = 18
+    GloryMajor = 19
