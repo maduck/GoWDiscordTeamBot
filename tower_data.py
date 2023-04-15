1848aa2e71f7417fe8bcae8abee09c48a0ecd9e3
maduck <utti+github@uttisoft.de>
2023-04-15 09:50:55 +0200

    refactor: tower data complexity

--- tower_data.py
+++ tower_data.py
@@ -207,58 +207,51 @@ class TowerOfDoomData:
 
         return ', '.join(rooms)
 
-    def format_output(self, guild, color, channel, prefix='!', _range=None, shortened=False):
-        my_data = self.get(guild)
+    def format_whole_floor(self, floor: int, guild_data, floor_data, shortened: bool):
+        if shortened:
+            return f'{self.format_short_floor(int(floor), floor_data)}'
+        return f'Floor {floor}: {self.format_floor(guild_data, int(floor), floor_data)}'
 
-        tower_data = my_data.get(str(channel.id), {}).items()
+    def format_output(self, guild, color, channel, prefix='!', _range=None, shortened=False):
+        guild_data = self.get(guild)
+
+        channel_data = guild_data.get(str(channel.id), {}).items()
         title = f'Tower of Doom overview for {channel}'
 
-        if not tower_data:
+        if not channel_data:
             e = discord.Embed(title=title, color=color)
             e.add_field(name='Failure',
                         value=f'Couldn\'t find any data for #{channel.name}.\n'
                               f'Please use `{prefix}towerhelp` for more info.')
             return e
 
-        tower_data = sorted(tower_data, key=operator.itemgetter(0))
+        channel_data = sorted(channel_data, key=operator.itemgetter(0))
         if _range:
             my_range = _range.split('-')
-            tower_data = [floor for floor in tower_data if int(my_range[0]) <= floor[0] <= int(my_range[1])]
-            if not tower_data:
+            channel_data = [floor for floor in channel_data if int(my_range[0]) <= floor[0] <= int(my_range[1])]
+            if not channel_data:
                 e = discord.Embed(title=title, color=color)
                 e.add_field(name='Failure', value=f'No data for floors {_range}.')
                 return e
 
-        display = {}
-        for key in my_data["rooms"].keys():
-            display[key] = my_data["rooms"][key][0]
-        for key in my_data["scrolls"].keys():
-            display[key] = my_data["scrolls"][key][0]
-
         e = discord.Embed(title=title, color=color)
 
         field_lines = []
-        starting_floor = tower_data[0][0]
+        starting_floor = channel_data[0][0]
         field_header = channel.name
-        for floor, floor_data in tower_data:
-            line = f'Floor {floor}: {self.format_floor(my_data, int(floor), floor_data)}'
-            if shortened:
-                line = f'{self.format_short_floor(int(floor), floor_data)}'
+        for floor, floor_data in channel_data:
+            line = self.format_whole_floor(int(floor), guild_data, floor_data, shortened)
             if len(field_header) + len(line) + sum([len(fl) + 1 for fl in field_lines]) < 1024:
                 field_lines.append(line)
             else:
-                tower_text = '\n'.join(field_lines)
-                if shortened:
-                    tower_text = '/'.join(field_lines)
+                tower_text = ('/' if shortened else '\n').join(field_lines)
                 e.add_field(name=field_header, value=tower_text, inline=False)
                 field_lines = [line]
                 starting_floor = floor
             field_header = f'Floors {starting_floor} - {floor}'
             if floor == starting_floor:
                 field_header = f'Floor {floor}'
-        tower_text = '\n'.join(field_lines)
-        if shortened:
-            tower_text = '/'.join(field_lines)
+        tower_text = ('/' if shortened else '\n').join(field_lines)
         e.add_field(name=field_header, value=tower_text, inline=False)
         return e
 
