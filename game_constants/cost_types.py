751b6374dfaf452f88a28d820d721fd2579b4d49
maduck <utti+github@uttisoft.de>
2024-01-07 10:11:58 +0100

    feat: update cost types from game files

--- game_constants/cost_types.py
+++ game_constants/cost_types.py
@@ -1,6 +1,11 @@
+# public enum GWShopData.CostTypes
 COST_TYPES = (
     'Cash',
     '[GEMS]',
     '[GLORY]',
+    '[WARCOINS]',
+    '[SILVERMARKS]',
+    '[GOLDMARKS]',
+    '[BURNINGMARKS]',
     '[TROOPHELP_COUNT0]',
 )
